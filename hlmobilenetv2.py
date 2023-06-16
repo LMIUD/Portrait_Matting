ce622ea56e9ea98627987a74434b5f10d9ad8c41
LMIUD <113013296+LMIUD@users.noreply.github.com>
2023-06-16 21:40:52 +0800

    Update hlmobilenetv2.py

--- hlmobilenetv2.py
+++ hlmobilenetv2.py
@@ -1,47 +1,12 @@
-"""
-This MobileNetV2 implementation is modified from the following repository:
-https://github.com/tonylins/pytorch-mobilenet-v2
-https://github.com/CSAILVision/semantic-segmentation-pytorch
-
-IndexNet Matting
-
-Indices Matter: Learning to Index for Deep Image Matting
-IEEE/CVF International Conference on Computer Vision, 2019
-
-This software is strictly limited to academic purposes only
-Copyright (c) 2019, Hao Lu (hao.lu@adelaide.edu.au)
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-  
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import os
 import sys
 import math
 from time import time
 from functools import partial
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
 
 from hlaspp import ASPP
 from lib.nn import SynchronizedBatchNorm2d
